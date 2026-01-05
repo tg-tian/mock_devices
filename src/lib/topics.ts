7d06e017aa5a151d8641ca4e439d305f1ec9da85
tgapk <914837973@qq.com>
2026-01-05 17:20:12 +0800

    Initial commit

---
+++ src/lib/topics.ts
@@ -0,0 +1,4 @@
+export const propertiesTopic = (deviceId: string) => `devices/${deviceId}/property`
+export const eventsTopic = (deviceId: string) => `devices/${deviceId}/events`
+export const commandsTopic = (deviceId: string) => `devices/${deviceId}/command`
+export const configTopic = (deviceId: string) => `devices/${deviceId}/config`
